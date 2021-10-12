a858eef0e3d7063aa9eaf168ee326ac5b8cc0920
tomascuetog <87738321+tomascuetog@users.noreply.github.com>
2021-10-12 19:43:59 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { NavBar } from './components/Navbar';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+export const NavBar= () =>{
+	return (
+	<div className="AppNavBar">
+	  <p className="NavBarInicio">
+		Inicio
+	  </p>
+	  <p className="NavBarServicios">
+		Servicios
+	  </p>
+	  <p className="NavBarNosotros">
+		Nosotros
+	  </p>
+	  <p className="NavBarContacto">
+		Contacto
+	  </p>
+	</div>
+	);
+  }
+  
